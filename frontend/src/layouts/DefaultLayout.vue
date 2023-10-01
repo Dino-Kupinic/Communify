f7241e416eb72cfae6793f88633ca7881e70e1c6
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-10-01 22:33:24 +0200

    added views

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+import Footer from "@/components/footer/Footer.vue"
+</script>
+
+<template>
+  <RouterView></RouterView>
+  <Footer></Footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Logo></Logo>
+</template>
+
+<style scoped>
+
+</style>
