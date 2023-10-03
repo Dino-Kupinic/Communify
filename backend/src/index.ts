b84bf746ddabb1cc5f396bb73870058adcf72b7c
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-10-03 15:21:52 +0200

    added errorHandler, express.json() and jsDoc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,15 +3,29 @@ import {Server} from "socket.io"
 import dotenv from "dotenv"
 import {createServer} from "node:http"
 import {ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData} from "./models/socket-io"
+import {errorHandler} from "./middleware/errorHandler"
 import {connectAndQuery} from "./database/db"
 
 dotenv.config()
 
 const app: Express = express()
+app.use(express.json())
+app.use(errorHandler)
+
+
 const port: string | undefined = process.env.PORT
 const FRONTEND_URL: string = "http://localhost:10000"
 
 const server = createServer(app)
+
+/**
+ * Represents an instance of a socket io server.
+ *
+ * @template ClientToServerEvents - The type of events sent from the client to the server.
+ * @template ServerToClientEvents - The type of events sent from the server to the client.
+ * @template InterServerEvents - The type of events exchanged between servers.
+ * @template SocketData - The type of data transmitted over sockets.
+ */
 const io: Server<
   ClientToServerEvents,
   ServerToClientEvents,
