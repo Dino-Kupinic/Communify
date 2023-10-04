6aaaa73879c323b735a1759b42a4846ee2e19fc9
Neuery17Alt <jannik.angerer@bmd.at>
2023-10-04 18:22:34 +0200

    added a UserProfileBar.vue-Component for the header in the chatrooms-list

---
+++ frontend/src/components/user/UserProfileBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  profilePicture?: String
+  username?: String
+}>()
+
+</script>
+
+<template>
+<div id="userbar-container-div">
+  <img id="user-profile" src="undefined">
+  <span id="username"> {{ username }}</span>
+</div>
+</template>
+
+<style scoped>
+#userbar-container-div {
+  width: 100%;
+  height: 6em;
+  background-color: var(--neutral-900);
+
+}
+</style>
