f88ca8f26ca6c32597dc09dc256e32bf99f85a0b
Neuery17Alt <jannik.angerer@bmd.at>
2023-10-04 17:22:38 +0200

    updated TitleText.vue, component for the title of the chatrooms

---
+++ frontend/src/components/text/TitleText.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string
+}>()
+
+</script>
+
+<template>
+  <p id="chatroom-title"> {{ title }} </p>
+</template>
+
+<style scoped>
+#chatroom-title {
+  font-size: 1em;
+}
+
+
+</style>
