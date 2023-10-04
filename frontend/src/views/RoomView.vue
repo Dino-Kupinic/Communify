512d58c8bbe781732b47e6d2b7f9e5aa94dd2270
Neuery17Alt <jannik.angerer@bmd.at>
2023-10-04 17:21:42 +0200

    updated RoomView.vue, added style for the chatrooms-list container as well as added examples for chatrooms with random names

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
 
+import RoomsList from "@/components/chatrooms/RoomsList.vue"
 </script>
 
 <template>
+  <div id="chatrooms-container">
+    <RoomsList title="Hallo1"></RoomsList>
+    <RoomsList title="Hallo2"></RoomsList>
+    <RoomsList title="Hallo3"></RoomsList>
+    <RoomsList title="SEW"></RoomsList>
+    <RoomsList title="ITP2"></RoomsList>
+    <RoomsList title="NWT"></RoomsList>
+    <RoomsList title="Deutsch"></RoomsList>
+    <RoomsList title="GGP"></RoomsList>
+  </div>
 
 </template>
 
 <style scoped>
+#chatrooms-container {
+  border: 1px solid var(--color-border-very-soft);
+  width: 25%;
+  overflow-y: scroll;
+  height: 95vh;
+}
+
+
+
 
 </style>
