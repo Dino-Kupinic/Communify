8b95a63d0bbfbc6dd8adf4cfed3277e415f0095c
Neuery17Alt <jannik.angerer@bmd.at>
2023-10-04 21:53:24 +0200

    updated UserProfileBar.vue, added html-elements and edited css

--- frontend/src/components/user/UserProfileBar.vue
+++ frontend/src/components/user/UserProfileBar.vue
@@ -1,24 +1,44 @@
 <script setup lang="ts">
 
+import UserIcon from "@/components/user/UserIcon.vue"
+import UserProfileText from "@/components/user/UserProfileText.vue"
+
 const props = defineProps<{
-  profilePicture?: String
   username?: String
 }>()
 
 </script>
 
 <template>
-<div id="userbar-container-div">
-  <img id="user-profile" src="undefined">
-  <span id="username"> {{ username }}</span>
-</div>
+  <div id="userbar-container-div">
+    <UserIcon imgPath="src/assets/img/github.svg"></UserIcon>
+    <UserProfileText id="username">
+      {{ username }}
+    </UserProfileText>
+  </div>
 </template>
 
 <style scoped>
 #userbar-container-div {
   width: 100%;
   height: 6em;
-  background-color: var(--neutral-900);
-
+  background-color: var(--color-background);
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid var(--color-border-soft);
 }
+
+#username {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+  width: 70%;
+}
+
+#iconContainer {
+  height: 100%;
+  width: min-content;
+  padding: 14.75px;
+}
+
 </style>
