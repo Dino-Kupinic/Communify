2ec190b1fd4ef548df6a5038699bcee5c2485537
Neuery17Alt <jannik.angerer@bmd.at>
2023-10-04 21:51:00 +0200

    updated RoomView, added all new html elements and edited css

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -2,31 +2,41 @@
 
 import RoomsList from "@/components/chatrooms/RoomsList.vue"
 import UserProfileBar from "@/components/user/UserProfileBar.vue"
+import ChatRoom from "@/components/chatrooms/ChatRoom.vue"
 
 
 </script>
 
 <template>
-  <div id="userbar-container">
-    <UserProfileBar username="Neuery17" profilePicture="undefined"></UserProfileBar>
+  <div id="Site-Container">
+    <div id="userbar-chatrooms-container">
+      <div id="userbar-container">
+        <UserProfileBar username="Neuery17"></UserProfileBar>
+      </div>
+
+      <div id="chatrooms-container">
+        <RoomsList title="Hallo1"></RoomsList>
+        <RoomsList title="Hallo2"></RoomsList>
+        <RoomsList title="Hallo3"></RoomsList>
+        <RoomsList title="SEW"></RoomsList>
+        <RoomsList title="ITP2"></RoomsList>
+        <RoomsList title="NWT"></RoomsList>
+        <RoomsList title="Deutsch"></RoomsList>
+        <RoomsList title="GGP"></RoomsList>
+        <RoomsList title="GGP"></RoomsList>
+        <RoomsList title="GGP"></RoomsList>
+        <RoomsList title="GGP"></RoomsList>
+        <RoomsList title="GGP"></RoomsList>
+        <RoomsList title="GGP"></RoomsList>
+        <RoomsList title="GGP"></RoomsList>
+        <RoomsList title="GGP "></RoomsList>
+      </div>
+    </div>
+
+    <ChatRoom></ChatRoom>
   </div>
-  <div id="chatrooms-container" @click="test">
-    <RoomsList title="Hallo1"></RoomsList>
-    <RoomsList title="Hallo2"></RoomsList>
-    <RoomsList title="Hallo3"></RoomsList>
-    <RoomsList title="SEW"></RoomsList>
-    <RoomsList title="ITP2"></RoomsList>
-    <RoomsList title="NWT"></RoomsList>
-    <RoomsList title="Deutsch"></RoomsList>
-    <RoomsList title="GGP"></RoomsList>
-    <RoomsList title="GGP"></RoomsList>
-    <RoomsList title="GGP"></RoomsList>
-    <RoomsList title="GGP"></RoomsList>
-    <RoomsList title="GGP"></RoomsList>
-    <RoomsList title="GGP"></RoomsList>
-    <RoomsList title="GGP"></RoomsList>
-    <RoomsList title="GGP "></RoomsList>
-  </div>
+
+
 
 </template>
 
@@ -34,19 +44,27 @@ import UserProfileBar from "@/components/user/UserProfileBar.vue"
 #chatrooms-container {
   overflow-y: scroll;
   height: 89.4vh;
-  width: 25%;
+  width: 100%;
 }
 
-#chatrooms-container, #userbar-container{
+#chatrooms-container, #userbar-container {
   border-right: 1px solid var(--color-border-very-soft);
+  display: flex;
+  flex-wrap: wrap;
 }
 
 #userbar-container {
-  width: 25%;
-  border-bottom: 1px solid var(--color-border-soft);
+  width: 100%;
+
 }
 
+#Site-Container {
+  display: flex;
+}
 
+#userbar-chatrooms-container {
+  width: 25%;
+}
 
 
 </style>
