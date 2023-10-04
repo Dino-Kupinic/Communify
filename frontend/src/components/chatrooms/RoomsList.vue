d0f9ed9c2773e209422f93bec0bd7d054bdadc28
Neuery17Alt <jannik.angerer@bmd.at>
2023-10-04 17:20:02 +0200

    updated RoomList.vue, added a stile for the chatroom-example and defined props

--- frontend/src/components/chatrooms/RoomsList.vue
+++ frontend/src/components/chatrooms/RoomsList.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
+import ChatRoom from "@/components/chatrooms/ChatRoom.vue"
+import Title from "@/components/text/TitleText.vue"
+import TitleText from "@/components/text/TitleText.vue"
+
+const props = defineProps<{
+  title?: string
+}>()
 
 
 </script>
 
 <template>
-
+    <div id="chatroom-div">
+      <TitleText :title="title"></TitleText>
+    </div>
 </template>
 
 <style scoped>
+#chatroom-div {
+  width: 100%;
+  height: 6em;
+  padding: 0.5%;
+  border-top: 1px solid var(--color-border-soft);
+  background-color: var(--neutral-900);
+}
+
+
+
 
 </style>
