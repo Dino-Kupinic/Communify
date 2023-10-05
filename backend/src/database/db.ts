40eea899ef4fa9f238216ed4a9146f96646d9857
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-10-05 20:16:21 +0200

    added jsdoc and small changes

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,12 +1,22 @@
-import {Client} from "pg"
+import {Pool, QueryResult, QueryResultRow} from "pg"
 
 const DATABASE_PORT: number | undefined = process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined
 const DATABASE_HOST: string = "database"
-const DATABASE_NAME: string = "chatapp"
+const DATABASE_NAME: string = "postgres"
 const DATABASE_USERNAME: string = "postgres"
 const DATABASE_PASSWORD: string = "123"
 
-export const client: Client = new Client({
+/**
+ * Represents a client for connecting to a database.
+ * @class
+ * @param {object} options - The options for configuring the client.
+ * @param {string} options.host - The hostname or IP address of the database server.
+ * @param {number} options.port - The port number of the database server.
+ * @param {string} options.database - The name of the database to connect to.
+ * @param {string} options.user - The username for authentication.
+ * @param {string} options.password - The password for authentication.
+ */
+export const pool: Pool = new Pool({
   host: DATABASE_HOST,
   port: DATABASE_PORT,
   database: DATABASE_NAME,
@@ -14,10 +24,16 @@ export const client: Client = new Client({
   password: DATABASE_PASSWORD,
 })
 
-
+/**
+ * Establishes a connection to the database and executes a query.
+ *
+ * @returns A promise that resolves to void.
+ */
 export async function connectAndQuery(): Promise<void> {
   try {
-    await client.connect()
+    await pool.connect()
+    const res: QueryResult<QueryResultRow> = await pool.query("SELECT $1::text as message", ["Hello world!"])
+    console.log(res.rows[0].message)
     console.log("Connected successfully to Database")
   } catch (err: unknown) {
     if (err instanceof Error) {
