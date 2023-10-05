2537fd5b71d4050813a4535f50f6d9caef3b7cab
Neuery17Alt <jannik.angerer@bmd.at>
2023-10-05 21:58:57 +0200

    updated UserProfileBar.vue, replaced everything with slots so this component can be used in multiple ways

--- frontend/src/components/user/UserProfileBar.vue
+++ frontend/src/components/user/UserProfileBar.vue
@@ -3,42 +3,14 @@
 import UserIcon from "@/components/user/UserIcon.vue"
 import UserProfileText from "@/components/user/UserProfileText.vue"
 
-const props = defineProps<{
-  username?: String
-}>()
+
 
 </script>
 
 <template>
-  <div id="userbar-container-div">
-    <UserIcon imgPath="src/assets/img/github.svg"></UserIcon>
-    <UserProfileText id="username">
-      {{ username }}
-    </UserProfileText>
-  </div>
+  <slot></slot>
 </template>
 
 <style scoped>
-#userbar-container-div {
-  width: 100%;
-  height: 6em;
-  background-color: var(--color-background);
-  display: flex;
-  flex-direction: row;
-  border-bottom: 1px solid var(--color-border-soft);
-}
-
-#username {
-  display: flex;
-  flex-wrap: wrap;
-  align-content: center;
-  width: 70%;
-}
-
-#iconContainer {
-  height: 100%;
-  width: min-content;
-  padding: 14.75px;
-}
 
 </style>
