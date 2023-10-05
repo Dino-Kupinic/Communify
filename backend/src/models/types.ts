51533330fbac42f94653368a99ab740ba47c25da
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-10-05 23:02:40 +0200

    made some room properties nullable

--- backend/src/models/types.ts
+++ backend/src/models/types.ts
@@ -1,8 +1,8 @@
 export interface Room {
   room_id: number
   name: string
-  maximum_users: number
-  description: string
-  password: string
+  maximum_users: number | null
+  description: string | null
+  password: string | null
   creator_id: number
 }
