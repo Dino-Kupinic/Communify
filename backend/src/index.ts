64c6465b6b70e178b7f41afb877112c72d101ae0
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-10-06 22:19:06 +0200

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,13 @@ import {createServer} from "node:http"
 import {errorHandler} from "./middleware/error-handler"
 import {connectAndQuery} from "./database/db"
 import {roomRouter} from "./room/room-controller"
+import cors from "cors"
 
 dotenv.config()
 
 export const port: string | undefined = process.env.PORT
 export const app: Express = express()
+app.use(cors())
 app.use(express.json())
 app.use(errorHandler)
 
