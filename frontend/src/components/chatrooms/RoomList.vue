c23ea6ecd502ab4a332afa2188632ff4e7a643a6
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-10-09 21:12:35 +0200

    added

---
+++ frontend/src/components/chatrooms/RoomList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  overflow-y: scroll;
+  height: 80.6vh;
+  width: 100%;
+  border-right: 1px solid var(--color-border-very-soft);
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
