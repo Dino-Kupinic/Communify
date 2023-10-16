0ca767ac22c0a385a74b5d6b55323cad39296a8c
Neuery17Alt <jannik.angerer@bmd.at>
2023-10-16 08:13:27 +0200

    updated RoomView.vue

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -12,9 +12,12 @@ import type {Room} from "@/model/types"
 import TitleText from "@/components/text/TitleText.vue"
 import ActionButton from "@/components/controls/ActionButton.vue"
 import Modal from "@/components/Boxes/Modal.vue"
+import InputField from "@/components/controls/InputField.vue"
 
 let username = ref("Neuery17")
 let rooms = ref<Room[]>()
+
+let privateRoom = ref(false)
 
 onMounted(async () => {
   try {
@@ -29,6 +32,10 @@ onMounted(async () => {
     console.error(err)
   }
 })
+
+function reversePswdDisplay (name : string) {
+  privateRoom.value = name === "private";
+}
 
 </script>
 
@@ -58,21 +65,41 @@ onMounted(async () => {
           <ActionButton height="max-content">
             <Icon image-name="account" file-extension="png"></Icon>
           </ActionButton>
-          <ActionButton height="max-content">
-            <Icon image-name="add" file-extension="png"></Icon>
-          </ActionButton>
+          <Modal modalTitle="Create Room">
+            <template #modal-btn>
+              <ActionButton height="max-content">
+                <Icon image-name="add" file-extension="png"></Icon>
+              </ActionButton>
+            </template>
+            <template #modal-content>
+              <InputField model-value="" label="Enter a Name for your Room"></InputField>
+              <!-- Private/Public Room Selection -->
+              <div id="selection-container-div">
+                <div class="selection-div">
+                  <input @click="reversePswdDisplay('public')" class="selection-input" type="radio" name="chatroom-status" id="public">
+                  <label class="selection-label" for="private">Public chat room</label>
+                </div>
+                <div class="selection-div">
+                  <input @click="reversePswdDisplay('private')" class="selection-input" type="radio" name="chatroom-status" id="private">
+                  <label class="selection-label" for="public">Private chat room</label>
+                  <InputField v-if="privateRoom" model-value="" label="Password" type="password"></InputField>
+                </div>
+              </div>
+            </template>
+            <template #second-btn>
+              <span id="save-btn">Save</span>
+            </template>
+          </Modal>
           <ActionButton height="max-content">
             <Icon image-name="settings" file-extension="png"></Icon>
           </ActionButton>
-
-
-
         </UserProfileBar>
       </div>
 
       <RoomList>
         <RoomContainer v-if="rooms" v-for="room in rooms" :title="room.name"></RoomContainer>
         <TitleText v-else title="Loading..."></TitleText>
+
         <Modal></Modal>
       </RoomList>
     </div>
@@ -147,6 +174,33 @@ onMounted(async () => {
   justify-content: center;
   align-content: center;
 }
+
+#selection-container-div {
+  display: block;
+}
+
+.selection-div {
+  margin-top: 5%;
+  padding-bottom: 1%;
+  padding-left: 1%;
+}
+
+.selection-input {
+  margin-right: 1%;
+}
+
+.selection-label {
+  font-size: 1.2rem;
+}
+
+#save-btn {
+  font-weight: bold;
+}
+
+
+
+
+
 
 
 </style>
