d465fbd0d824259309783fc0292ff23619236af8
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-10-27 00:02:19 +0200

    extracted modal

--- frontend/src/views/ChatRoomsView.vue
+++ frontend/src/views/ChatRoomsView.vue
@@ -2,69 +2,15 @@
 import RoomContainer from "@/components/chatrooms/RoomContainer.vue"
 import UserProfileBar from "@/components/user/UserProfileBar.vue"
 import ChatRoom from "@/components/chatrooms/ChatRoom.vue"
-import {onMounted, reactive, ref} from "vue"
+import {onMounted, ref} from "vue"
 import RoomList from "@/components/chatrooms/RoomList.vue"
 import Icon from "@/components/util/Icon.vue"
-import type {Client, Room} from "@/model/types"
+import type {Room} from "@/model/types"
 import TitleText from "@/components/text/TitleText.vue"
 import ActionButton from "@/components/controls/ActionButton.vue"
-import Modal from "@/components/Boxes/Modal.vue"
-import InputField from "@/components/controls/InputField.vue"
-import BodyText from "@/components/text/BodyText.vue"
 import {socket} from "@/socket/server"
 import {useRoomStore} from "@/stores/roomStore"
-import {maxLength, maxValue, minValue, required} from "@vuelidate/validators"
-import {
-  DESCRIPTION_MAX_LENGTH,
-  MAXIMUM_USERS,
-  MINIMUM_USERS,
-  ROOM_NAME_MAX_LENGTH, ROOM_PASSWORD_MAX_LENGTH,
-} from "@/model/type_constants"
-import useVuelidate from "@vuelidate/core"
-import {fetchData} from "@/model/util-functions"
-
-const initialState: Room = reactive({
-  room_id: null,
-  name: "",
-  maximum_users: null,
-  description: null,
-  password: null,
-  creator_id: 0,
-})
-
-const state = reactive<Room>({...initialState})
-
-const rules = {
-  name: {
-    required,
-    maxLength: maxLength(ROOM_NAME_MAX_LENGTH),
-  },
-  maximum_users: {
-    minValue: minValue(MINIMUM_USERS),
-    maxValue: maxValue(MAXIMUM_USERS),
-  },
-  description: {
-    maxLength: maxLength(DESCRIPTION_MAX_LENGTH),
-  },
-  password: {
-    maxLength: maxLength(ROOM_PASSWORD_MAX_LENGTH),
-  },
-}
-
-const v$ = useVuelidate(rules, state)
-
-function resetState() {
-  Object.assign(state, initialState)
-}
-
-async function getCurrentUserId(): Promise<number> {
-  const token = localStorage.getItem("auth_token")
-  if (token) {
-    const client: Client = await fetchData("http://localhost:4000/auth/profile", "GET", [["access_token", token]])
-    return client.user_id as number
-  }
-  throw new Error("User not authorized")
-}
+import CreateRoomModal from "@/components/modals/CreateRoomModal.vue"
 
 const rooms = ref<Room[]>()
 const roomStore = useRoomStore()
@@ -74,31 +20,6 @@ onMounted(async () => {
   await roomStore.fetchRooms()
   rooms.value = roomStore.rooms
 })
-
-async function createRoom() {
-  const isFormCorrect = await v$.value.$validate()
-  if (!isFormCorrect) return
-
-  const creator_id = await getCurrentUserId()
-  const room: Room = {
-    room_id: null,
-    name: state.name,
-    maximum_users: state.maximum_users,
-    description: state.description,
-    password: state.password,
-    creator_id: creator_id,
-  }
-
-  try {
-    await roomStore.addRoom(room)
-    rooms.value = roomStore.rooms
-    resetState()
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-let isPrivateRoom = ref<boolean>(false)
 
 async function refreshRooms() {
   await roomStore.fetchRooms()
@@ -112,12 +33,12 @@ const actionButtons = ref([
   {icon: "settings", label: "Settings"},
 ])
 
-function reverseDisplay(name: string) {
-  isPrivateRoom.value = name === "private"
-}
-
 function joinRoom(room: string) {
   console.log(room)
+}
+
+function updateOnRoomCreation() {
+  rooms.value = roomStore.rooms
 }
 
 </script>
@@ -138,41 +59,11 @@ function joinRoom(room: string) {
             class="logout"
             @click="button.action"
           >
-            <Modal v-if="button.icon ==='add'" modalTitle="Create Room">
-              <template #modal-btn>
-                <Icon class="img" :image-name="button.icon" file-extension="png"/>
-                <BodyText class="btn-span">{{ button.label }}</BodyText>
-              </template>
-              <template #modal-content>
-                <InputField v-model="state.name" label="Enter a Name for your Room"></InputField>
-                <InputField v-model="state.description" label="Description"></InputField>
-                <InputField v-model="state.maximum_users" label="Max. User" id="max-user-input" type="number" value="1"
-                            min="1"></InputField>
-                <!-- Private/Public Room Selection -->
-                <div id="selection-container-div">
-                  <div class="selection-div">
-                    <input @click="reverseDisplay('public')" class="selection-input" type="radio"
-                           name="chatroom-status" id="public">
-                    <label class="selection-label" for="private">Public chat room</label>
-                  </div>
-                  <div class="selection-div">
-                    <input @click="reverseDisplay('private')" class="selection-input" type="radio"
-                           name="chatroom-status" id="private">
-                    <label class="selection-label" for="public">Private chat room</label>
-                    <InputField v-if="isPrivateRoom" v-model="state.password" label="Password"
-                                type="password"></InputField>
-                  </div>
-                </div>
-              </template>
-              <template #second-btn>
-                <span @click="createRoom" id="save-btn">Save</span>
-              </template>
-            </Modal>
+            <CreateRoomModal @created="updateOnRoomCreation" v-if="button.icon ==='add'" modalTitle="Create Room"></CreateRoomModal>
             <template v-else>
               <Icon class="img" :image-name="button.icon" file-extension="png"/>
               <span class="btn-span">{{ button.label }}</span>
             </template>
-
           </ActionButton>
         </div>
       </div>
@@ -237,32 +128,5 @@ function joinRoom(room: string) {
   flex-wrap: wrap;
   align-content: center;
 }
-
-#selection-container-div {
-  display: block;
-  margin-bottom: 5%;
-}
-
-.selection-div {
-  margin-top: 5%;
-  padding-left: 1%;
-}
-
-.selection-input {
-  margin-right: 1%;
-}
-
-.selection-label {
-  font-size: 1.2rem;
-}
-
-#save-btn {
-  font-weight: bold;
-}
-
-#max-user-input {
-  width: 50%;
-}
-
 
 </style>
