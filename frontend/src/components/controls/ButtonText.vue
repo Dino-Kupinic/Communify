0b77802ab184478ba1fed29c65a1d1180c06f7db
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-10-28 01:00:09 +0200

    added ButtonText.vue, added Buttontext with simply span so every button looks the same

---
+++ frontend/src/components/controls/ButtonText.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <span>
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped>
+span {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+}
+</style>
