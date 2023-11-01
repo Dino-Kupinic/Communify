d8550bec64f36b220dd7fd99ab6da1347eaaac53
JKerbl <117641110+JKerbl@users.noreply.github.com>
2023-11-02 00:39:00 +0100

    Added number input component with default placeholder

---
+++ frontend/src/components/boxes/CodeInputBox.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface Props {
+  placeholder?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  placeholder: "NUM?",
+})
+
+</script>
+
+<template>
+  <div class="form-group">
+    <div id="formBorder">
+      <input :placeholder="placeholder">
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+.form-group {
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+
+  input {
+    -webkit-backdrop-filter: blur(15px);
+    backdrop-filter: blur(15px);
+    border: 1px solid var(--neutral-700);
+    border-radius: 5px;
+    text-align: center;
+    justify-content: center;
+    align-content: center;
+    height: 4em;
+    width: 4em;
+    margin: 5px 5%;
+    max-width: 4em;
+    max-height: 4em;
+    font-weight: 550;
+  }
+</style>
