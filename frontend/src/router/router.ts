ac458f8fcbacc11880e23239a8e114daf6611412
JKerbl <117641110+JKerbl@users.noreply.github.com>
2023-11-02 00:38:11 +0100

    Refactored the Name of the ResetPassword-Window

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -16,7 +16,7 @@ import DefaultLayout from "@/layouts/DefaultLayout.vue"
 import UserHomeView from "@/views/UserHomeView.vue"
 import UserProfileView from "@/views/UserProfileView.vue"
 import ChatRoomsView from "@/views/ChatRoomsView.vue"
-import ResetPassword from "@/components/auth/ResetPassword.vue"
+import ResetPassword from "@/components/auth/ResetPasswordWindow.vue"
 
 const routes: Array<RouteRecordRaw> & {
   meta?: RouteMeta
