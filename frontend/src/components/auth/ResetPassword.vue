58b39a2fcefe8ae1c741c7fefd4f1612ccb78f5f
JKerbl <117641110+JKerbl@users.noreply.github.com>
2023-11-01 22:02:49 +0100

    Implemented component imports and added styling for the password reset section. It includes a bigger Subtitle text and a describtion for the user

--- frontend/src/components/auth/ResetPassword.vue
+++ frontend/src/components/auth/ResetPassword.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 
+import BodySubtitleText from "@/components/text/BodySubtitleText.vue"
+import BodyText from "@/components/text/BodyText.vue"
 </script>
 
 <template>
-  <p>Hallo!</p>
+  <BodySubtitleText id="body-subtitle-text-style">
+      Password Reset Support
+  </BodySubtitleText>
+  <BodyText>
+      Please enter your username and email adress in the input field below.<br>
+  </BodyText>
 </template>
 
 <style scoped>
-
+  #body-subtitle-text-style{
+    margin-top: 3rem;
+  }
 </style>
