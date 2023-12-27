1558763df7e07ad0e41d0e733d6fecdb7965c781
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-27 20:51:56 +0100

    working on complete refactor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,8 @@
 <script setup lang="ts">
-import {onMounted} from "vue"
 
-onMounted(() => {
-  if (!localStorage.getItem("auth_token"))
-    localStorage.setItem("auth_token", "")
-})
 </script>
 
 <template>
-  <RouterView></RouterView>
+
 </template>
 
-<style scoped>
-</style>
