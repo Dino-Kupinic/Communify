bfa9e9066ea433fd0e9d1e594c0b211f86088f0f
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-30 23:03:58 +0100

    working on hero text

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<p>Home</p>
+  <p class="text-6xl bg-gradient-to-r from-blue-500 to-blue-800 bg-clip-text text-transparent">Let's start chatting.</p>
 </template>
 
 <style scoped>
