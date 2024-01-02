8ebea281624a56fd09464759e9970cbd17c48956
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-02 22:49:41 +0100

    created

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import {Button} from "@/components/ui/button"
+</script>
+
+<template>
+  <div class="text-center h-screen w-full mt-20">
+    <p class="text-7xl font-bold text-primary p-5">404</p>
+    <p class="text-lg">We can't find what you are looking for!</p>
+    <RouterLink to="/">
+      <Button class="mt-5 w-1/2 sm:w-24">Home</Button>
+    </RouterLink>
+  </div>
+</template>
+
