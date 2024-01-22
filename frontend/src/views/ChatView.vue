9dcb0fad5655e62211e3d3c4630adff597884dfb
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-22 21:56:01 +0100

    added ChatView, WIP

---
+++ frontend/src/views/ChatView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router"
+import {useRoomStore} from "@/stores/roomStore.ts"
+import {ref, watch} from "vue"
+import {Room} from "@/model/room.dto.ts"
+import ChatHeader from "@/components/chat/ChatHeader.vue"
+
+const route = useRoute()
+const params = route.params
+
+const roomStore = useRoomStore()
+const room = ref<Room | undefined>(await roomStore.getRoomById(params.room as string))
+</script>
+
+<template>
+  <div class="h-full">
+    <ChatHeader :title="room?.name"/>
+  </div>
+</template>
+
