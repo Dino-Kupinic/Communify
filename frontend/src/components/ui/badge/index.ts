053dfa1a71534a6f1227ac67141f8a34c5bff02e
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-22 09:41:05 +0100

    added more color options to badges

--- frontend/src/components/ui/badge/index.ts
+++ frontend/src/components/ui/badge/index.ts
@@ -3,7 +3,7 @@ import { cva } from 'class-variance-authority'
 export { default as Badge } from './Badge.vue'
 
 export const badgeVariants = cva(
-  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
@@ -14,6 +14,11 @@ export const badgeVariants = cva(
         destructive:
           'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline: 'text-foreground',
+        red: 'border-transparent bg-red-500 text-primary-foreground hover:bg-primary/80',
+        yellow: 'border-transparent bg-yellow-500 text-primary-foreground hover:bg-primary/80',
+        green: 'border-transparent bg-green-500 text-primary-foreground hover:bg-primary/80',
+        blue: 'border-transparent bg-blue-500 text-primary-foreground hover:bg-primary/80',
+        purple: 'border-transparent bg-purple-500 text-primary-foreground hover:bg-primary/80',
       },
     },
     defaultVariants: {
